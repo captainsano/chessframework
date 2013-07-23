0c694838bc784ee0c15701c38e0aee9e2b5bcfa3
Santhos Baala RS <rssanthosbaala@gmail.com>
2013-07-23 16:08:05 +0530

    Rewritten a few tests to use the CPPAssert* macros. Corrected errors in a few tests.

--- ChessFramework/ChessFramework Tests/Square_Tests.mm
+++ ChessFramework/ChessFramework Tests/Square_Tests.mm
@@ -7,7 +7,12 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "CPPTest.h"
+
 #import <vector>
+#import "Square.h"
+
+using sfc::cfw::Square;
 
 @interface Square_Tests : XCTestCase
 
@@ -27,20 +32,20 @@
 
 - (void)testThatDefaultSquareHasZeroValue {
     Square s;
-    XCTAssertEquals(std::static_cast<int>(s), (int)0, @"Default initialized square should have zero as index");
+    XCTAssertTrue(s.getIndex() == 0, @"Default initialized square should have zero as index");
 }
 
 - (void)testInvalidFileAndRankValuesThrowOutOfRangeException {
-    XCTAssertThrows(Square s(8, 9), @"Both file and rank out of range. Should throw an exception");
-    XCTAssertThrows(Square s(5, 9), @"File in range, rank out of range. Should throw an exception");
-    XCTAssertThrows(Square s(9, 5), @"File out of range, rank in range. Should throw an exception");
-    XCTAssertThrows(Square s(SHRT_MAX, SHRT_MAX), @"Both file and rank out of range, maximum. Should throw an exception");
+    CPPAssertThrows(new Square(8, 9), @"Both file and rank out of range. Should throw an exception");
+    CPPAssertThrows(new Square(5, 9), @"File in range, rank out of range. Should throw an exception");
+    CPPAssertThrows(new Square(9, 5), @"File out of range, rank in range. Should throw an exception");
+    CPPAssertThrows(new Square(SHRT_MAX, SHRT_MAX), @"Both file and rank out of range, maximum. Should throw an exception");
 }
 
 - (void)testDoesNotThrowExceptionForFileAndRankValuesInRange {
     for (unsigned short file = 0; file < 8; file++) {
         for (unsigned short rank = 0; rank < 8; rank++) {
-            XCTAssertNoThrow(Square s(file, rank), @"Both file and rank in range. Should not throw an exception");.
+            CPPAssertNoThrow(new Square(file, rank), @"Both file and rank in range. Should not throw an exception");
         }
     }
 }
@@ -56,13 +61,13 @@
 }
 
 - (void)testInvalidIndexValuesThrowOutOfRangeException {
-    XCTAssertThrows(Square s(64), @"Should throw out of range exception");
-    XCTAssertThrows(Square s(SHRT_MAX), @"Should throw out of range exception");
+    CPPAssertThrows(new Square(64), @"Should throw out of range exception");
+    CPPAssertThrows(new Square(SHRT_MAX), @"Should throw out of range exception");
 }
 
 - (void)testDoesNotThrowExceptionForIndexValuesInRange {
     for (unsigned short i = 0; i < 64; i++) {
-        XCTAssertNoThrow(Square s(i), @"Should not throw an exception.");
+        XCTAssertNoThrow(new Square(i), @"Should not throw an exception.");
     }
 }
 
@@ -83,45 +88,48 @@
         labels.push_back(s.getLabel());
     }
     
-    XCTAssertTrue(labels[0] == "a1", @"Labels should match"); XCTAssertTrue(labels[1] == "b1", @"Labels should match");
-    XCTAssertTrue(labels[2] == "c1", @"Labels should match"); XCTAssertTrue(labels[3] == "d1", @"Labels should match");
-    XCTAssertTrue(labels[4] == "e1", @"Labels should match"); XCTAssertTrue(labels[5] == "f1", @"Labels should match");
-    XCTAssertTrue(labels[6] == "g1", @"Labels should match"); XCTAssertTrue(labels[7] == "h1", @"Labels should match");
-    
-    XCTAssertTrue(labels[8] == "a2", @"Labels should match"); XCTAssertTrue(labels[9] == "b2", @"Labels should match");
-    XCTAssertTrue(labels[10] == "c2", @"Labels should match"); XCTAssertTrue(labels[11] == "d2", @"Labels should match");
-    XCTAssertTrue(labels[12] == "e2", @"Labels should match"); XCTAssertTrue(labels[13] == "f2", @"Labels should match");
-    XCTAssertTrue(labels[14] == "g2", @"Labels should match"); XCTAssertTrue(labels[15] == "h2", @"Labels should match");
-    
-    XCTAssertTrue(labels[16] == "a3", @"Labels should match"); XCTAssertTrue(labels[17] == "b3", @"Labels should match");
-    XCTAssertTrue(labels[18] == "c3", @"Labels should match"); XCTAssertTrue(labels[19] == "d3", @"Labels should match");
-    XCTAssertTrue(labels[20] == "e3", @"Labels should match"); XCTAssertTrue(labels[21] == "f3", @"Labels should match");
-    XCTAssertTrue(labels[22] == "g3", @"Labels should match"); XCTAssertTrue(labels[23] == "h3", @"Labels should match");
-    
-    XCTAssertTrue(labels[24] == "a4", @"Labels should match"); XCTAssertTrue(labels[25] == "b4", @"Labels should match");
-    XCTAssertTrue(labels[26] == "c4", @"Labels should match"); XCTAssertTrue(labels[27] == "d4", @"Labels should match");
-    XCTAssertTrue(labels[28] == "e4", @"Labels should match"); XCTAssertTrue(labels[29] == "f4", @"Labels should match");
-    XCTAssertTrue(labels[30] == "g4", @"Labels should match"); XCTAssertTrue(labels[31] == "h4", @"Labels should match");
-    
-    XCTAssertTrue(labels[32] == "a5", @"Labels should match"); XCTAssertTrue(labels[33] == "b5", @"Labels should match");
-    XCTAssertTrue(labels[34] == "c5", @"Labels should match"); XCTAssertTrue(labels[35] == "d5", @"Labels should match");
-    XCTAssertTrue(labels[36] == "e5", @"Labels should match"); XCTAssertTrue(labels[37] == "f5", @"Labels should match");
-    XCTAssertTrue(labels[38] == "g5", @"Labels should match"); XCTAssertTrue(labels[39] == "h5", @"Labels should match");
-    
-    XCTAssertTrue(labels[40] == "a6", @"Labels should match"); XCTAssertTrue(labels[41] == "b6", @"Labels should match");
-    XCTAssertTrue(labels[42] == "c6", @"Labels should match"); XCTAssertTrue(labels[43] == "d6", @"Labels should match");
-    XCTAssertTrue(labels[44] == "e6", @"Labels should match"); XCTAssertTrue(labels[45] == "f6", @"Labels should match");
-    XCTAssertTrue(labels[46] == "g6", @"Labels should match"); XCTAssertTrue(labels[47] == "h6", @"Labels should match");
-    
-    XCTAssertTrue(labels[48] == "a7", @"Labels should match"); XCTAssertTrue(labels[49] == "b7", @"Labels should match");
-    XCTAssertTrue(labels[50] == "c7", @"Labels should match"); XCTAssertTrue(labels[51] == "d7", @"Labels should match");
-    XCTAssertTrue(labels[52] == "e7", @"Labels should match"); XCTAssertTrue(labels[53] == "f7", @"Labels should match");
-    XCTAssertTrue(labels[54] == "g7", @"Labels should match"); XCTAssertTrue(labels[55] == "h7", @"Labels should match");
-    
-    XCTAssertTrue(labels[56] == "a8", @"Labels should match"); XCTAssertTrue(labels[57] == "b8", @"Labels should match");
-    XCTAssertTrue(labels[58] == "c8", @"Labels should match"); XCTAssertTrue(labels[59] == "d8", @"Labels should match");
-    XCTAssertTrue(labels[60] == "e8", @"Labels should match"); XCTAssertTrue(labels[61] == "f8", @"Labels should match");
-    XCTAssertTrue(labels[62] == "g8", @"Labels should match"); XCTAssertTrue(labels[63] == "h8", @"Labels should match");
+    // Tests
+    {
+        XCTAssertTrue(labels[0] == "a1", @"Labels should match"); XCTAssertTrue(labels[1] == "b1", @"Labels should match");
+        XCTAssertTrue(labels[2] == "c1", @"Labels should match"); XCTAssertTrue(labels[3] == "d1", @"Labels should match");
+        XCTAssertTrue(labels[4] == "e1", @"Labels should match"); XCTAssertTrue(labels[5] == "f1", @"Labels should match");
+        XCTAssertTrue(labels[6] == "g1", @"Labels should match"); XCTAssertTrue(labels[7] == "h1", @"Labels should match");
+        
+        XCTAssertTrue(labels[8] == "a2", @"Labels should match"); XCTAssertTrue(labels[9] == "b2", @"Labels should match");
+        XCTAssertTrue(labels[10] == "c2", @"Labels should match"); XCTAssertTrue(labels[11] == "d2", @"Labels should match");
+        XCTAssertTrue(labels[12] == "e2", @"Labels should match"); XCTAssertTrue(labels[13] == "f2", @"Labels should match");
+        XCTAssertTrue(labels[14] == "g2", @"Labels should match"); XCTAssertTrue(labels[15] == "h2", @"Labels should match");
+        
+        XCTAssertTrue(labels[16] == "a3", @"Labels should match"); XCTAssertTrue(labels[17] == "b3", @"Labels should match");
+        XCTAssertTrue(labels[18] == "c3", @"Labels should match"); XCTAssertTrue(labels[19] == "d3", @"Labels should match");
+        XCTAssertTrue(labels[20] == "e3", @"Labels should match"); XCTAssertTrue(labels[21] == "f3", @"Labels should match");
+        XCTAssertTrue(labels[22] == "g3", @"Labels should match"); XCTAssertTrue(labels[23] == "h3", @"Labels should match");
+        
+        XCTAssertTrue(labels[24] == "a4", @"Labels should match"); XCTAssertTrue(labels[25] == "b4", @"Labels should match");
+        XCTAssertTrue(labels[26] == "c4", @"Labels should match"); XCTAssertTrue(labels[27] == "d4", @"Labels should match");
+        XCTAssertTrue(labels[28] == "e4", @"Labels should match"); XCTAssertTrue(labels[29] == "f4", @"Labels should match");
+        XCTAssertTrue(labels[30] == "g4", @"Labels should match"); XCTAssertTrue(labels[31] == "h4", @"Labels should match");
+        
+        XCTAssertTrue(labels[32] == "a5", @"Labels should match"); XCTAssertTrue(labels[33] == "b5", @"Labels should match");
+        XCTAssertTrue(labels[34] == "c5", @"Labels should match"); XCTAssertTrue(labels[35] == "d5", @"Labels should match");
+        XCTAssertTrue(labels[36] == "e5", @"Labels should match"); XCTAssertTrue(labels[37] == "f5", @"Labels should match");
+        XCTAssertTrue(labels[38] == "g5", @"Labels should match"); XCTAssertTrue(labels[39] == "h5", @"Labels should match");
+        
+        XCTAssertTrue(labels[40] == "a6", @"Labels should match"); XCTAssertTrue(labels[41] == "b6", @"Labels should match");
+        XCTAssertTrue(labels[42] == "c6", @"Labels should match"); XCTAssertTrue(labels[43] == "d6", @"Labels should match");
+        XCTAssertTrue(labels[44] == "e6", @"Labels should match"); XCTAssertTrue(labels[45] == "f6", @"Labels should match");
+        XCTAssertTrue(labels[46] == "g6", @"Labels should match"); XCTAssertTrue(labels[47] == "h6", @"Labels should match");
+        
+        XCTAssertTrue(labels[48] == "a7", @"Labels should match"); XCTAssertTrue(labels[49] == "b7", @"Labels should match");
+        XCTAssertTrue(labels[50] == "c7", @"Labels should match"); XCTAssertTrue(labels[51] == "d7", @"Labels should match");
+        XCTAssertTrue(labels[52] == "e7", @"Labels should match"); XCTAssertTrue(labels[53] == "f7", @"Labels should match");
+        XCTAssertTrue(labels[54] == "g7", @"Labels should match"); XCTAssertTrue(labels[55] == "h7", @"Labels should match");
+        
+        XCTAssertTrue(labels[56] == "a8", @"Labels should match"); XCTAssertTrue(labels[57] == "b8", @"Labels should match");
+        XCTAssertTrue(labels[58] == "c8", @"Labels should match"); XCTAssertTrue(labels[59] == "d8", @"Labels should match");
+        XCTAssertTrue(labels[60] == "e8", @"Labels should match"); XCTAssertTrue(labels[61] == "f8", @"Labels should match");
+        XCTAssertTrue(labels[62] == "g8", @"Labels should match"); XCTAssertTrue(labels[63] == "h8", @"Labels should match");
+    }
 }
 
 @end
