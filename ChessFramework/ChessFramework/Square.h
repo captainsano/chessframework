956d18309e753909630ee0b4237bb38cddc630a9
Santhos Baala RS <rssanthosbaala@gmail.com>
2013-07-23 21:14:58 +0530

    Added some basic documentation to the Square class.

--- ChessFramework/ChessFramework/Square.h
+++ ChessFramework/ChessFramework/Square.h
@@ -2,8 +2,9 @@
 //  Square.h
 //  ChessFramework
 //
-//  Created by Santhos Baala RS on 23/07/13.
-//  Copyright (c) 2013 64cloud. All rights reserved.
+/// @author Santhos Baala RS
+/// @copyright (c) 2013 64cloud. All rights reserved.
+/// @version 0.1
 //
 
 #ifndef __ChessFramework__Square__
@@ -14,16 +15,27 @@
 namespace sfc {
     namespace cfw {
         
+		/*! 
+		 @class Square
+		 @brief Represents any one of the 64 squares on the chess board.
+		 @discussion An index value 0 to 63 mapped to a valid square on 
+		 the chess board is represented by this class. The class can be
+		 converted to unsigned short and individual rank and file of 
+		 the represented square can also be obtained. Additionaly, for 
+		 convenience, it also provides a method for getting the string 
+		 label according to SAN conventions.
+		 */
         class Square {
             unsigned short index = 0;
             
         public:
-            Square() = default;
+			Square() = default;
             Square(unsigned short aIndex);
             Square(unsigned short aFile, unsigned short aRank);
             
-            operator unsigned short() const;
-            unsigned short  getFile() const;
+			operator unsigned short() const;
+            
+			unsigned short  getFile() const;
             unsigned short  getRank() const;
             unsigned short  getIndex() const;
             std::string     getLabel() const;
