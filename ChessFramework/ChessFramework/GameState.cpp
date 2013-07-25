3e5a3016af4694da25fd190308f564fc2f8a8282
Santhos Baala RS <rssanthosbaala@gmail.com>
2013-07-25 19:17:09 +0530

    Added the GameState class.

--- ChessFramework/ChessFramework/GameState.cpp
+++ ChessFramework/ChessFramework/GameState.cpp
@@ -6,4 +6,16 @@
 //  Copyright (c) 2013 64cloud. All rights reserved.
 //
 
+#include <exception>
+#include <utility>
+
 #include "GameState.h"
+#include "BitboardPosition.h"
+
+sfc::cfw::GameState::GameState(std::string && FENString,
+							   const sfc::cfw::Color & aSideToMove,
+							   const std::string castlingOptions,
+							   const sfc::cfw::Square & aEnpassantTarget
+							   ) {
+	this->position = std::make_shared<sfc::cfw::BitboardPosition>(std::forward<std::string>(FENString));
+}
--- ChessFramework/ChessFramework/GameState.h
+++ ChessFramework/ChessFramework/GameState.h
@@ -2,13 +2,41 @@
 //  GameState.h
 //  ChessFramework
 //
-//  Created by Santhosbaala RS on 25/07/13.
-//  Copyright (c) 2013 64cloud. All rights reserved.
+/// @author Santhos Baala RS
+/// @copyright (c) 2013 64cloud. All rights reserved.
+/// @version 0.1
 //
 
 #ifndef __ChessFramework__GameState__
 #define __ChessFramework__GameState__
 
-#include <iostream>
+#include <string>
+#include <memory>
+#include "Piece.h"
+#include "Square.h"
+#include "Position.h"
+
+namespace sfc {
+	namespace cfw {
+		class GameState {
+			std::shared_ptr<Position>	position			= nullptr;
+			sfc::cfw::Color				sideToMove			= sfc::cfw::ColorWhite;
+			sfc::cfw::Square			enpassantTarget		= sfc::cfw::Square(0);
+			char						castlingOptions[4]	= {'-', '-', '-', '-'};
+			
+			int							whiteKingStatus		= 0;	/// @todo Change type to king status enum
+			int							blackKingStatus		= 0;	/// @todo Change type to king status enum
+			
+		public:
+			// Default instantiation is prevented by ambiguity introduced
+			// due to the ambiguity introduced by the other constructor
+			GameState(std::string && FENString = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR",
+					  const sfc::cfw::Color & aSideToMove = sfc::cfw::ColorWhite,
+					  const std::string castlingOptions = "----",
+					  const sfc::cfw::Square & aEnpassantTarget = sfc::cfw::Square(0)
+					  );
+		};
+	}
+}
 
 #endif /* defined(__ChessFramework__GameState__) */
