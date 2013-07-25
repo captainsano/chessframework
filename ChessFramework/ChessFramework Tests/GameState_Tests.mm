34f5d83e27fb82e462eefd00129e5023e899b342
Santhos Baala RS <rssanthosbaala@gmail.com>
2013-07-25 19:16:19 +0530

    LOL! A little mixup of Objective-C and C++ test cases! Fixed all of those. Removed a test case that checks default instantiation of GameState as GameState design changed to prevent default instantiation.

--- ChessFramework/ChessFramework Tests/GameState_Tests.mm
+++ ChessFramework/ChessFramework Tests/GameState_Tests.mm
@@ -9,6 +9,7 @@
 #import <XCTest/XCTest.h>
 #import "Square.h"
 #import "Piece.h"
+#import "GameState.h"
 
 using sfc::cfw::Square;
 using sfc::cfw::GameState;
@@ -29,86 +30,82 @@ using sfc::cfw::GameState;
     [super tearDown];
 }
 
-- testDefaultInitializationDoesNotThrow() {
-	CPPAssertNoThrow(new GameState(), @"Default initialization of GameState should not throw");
-}
-
-- testInitializationWithValidPositionAndWhiteSideDoesNotThrow() {
+- (void)testInitializationWithValidPositionAndWhiteSideDoesNotThrow {
 	CPPAssertNoThrow(new GameState("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR", sfc::cfw::ColorWhite), @"Valid Initialization with White to play should not throw");
 }
 
-- testInitializationWithValidPositionAndBlackSideDoesNotThrow() {
+- (void)testInitializationWithValidPositionAndBlackSideDoesNotThrow {
 	CPPAssertNoThrow(new GameState("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR", sfc::cfw::ColorBlack), @"Valid Initialization with Black to play should not throw");
 }
 
-- testInitializationWithValidPositionEnpassantTargetAndCastlingOptionsDoesNotThrow() {
+- (void)testInitializationWithValidPositionEnpassantTargetAndCastlingOptionsDoesNotThrow{
 	CPPAssertNoThrow(new GameState("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR", sfc::cfw::ColorBlack, "KQkq", Square("e3")), @"Valid Initialization with enPassant target should not throw");
 	CPPAssertNoThrow(new GameState("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR", sfc::cfw::ColorBlack, "HAha", Square("e3")), @"Valid Initialization with enPassant target should not throw");
 }
 
-- testInitializationWithInvalidEnpassantTarget1Throws() {
+- (void)testInitializationWithInvalidEnpassantTarget1Throws {
 	CPPAssertThrows(new GameState("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR", sfc::cfw::ColorWhite, "----", Square("e6")), @"Invalid enpassant target should throw");
 }
 
-- testInitializationWithInvalidEnpassantTarget2Throws() {
+- (void)testInitializationWithInvalidEnpassantTarget2Throws {
 	CPPAssertThrows(new GameState("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR", sfc::cfw::ColorBlack, "----", Square("e3")), @"Invalid enpassant target should throw");
 }
 
-- testInitializationWithEnpassantTargetColorMismatchThrows1() {
+- (void)testInitializationWithEnpassantTargetColorMismatchThrows1 {
 	CPPAssertThrows(new GameState("rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR", sfc::cfw::ColorBlack, "----", Square("e6")), @"Invalid enpassant target should throw");
 }
 
-- testInitializationWithEnpassantTargetColorMismatchThrows2() {
+- (void)testInitializationWithEnpassantTargetColorMismatchThrows2 {
 	CPPAssertThrows(new GameState("rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR", sfc::cfw::ColorWhite, "----", Square("e3")), @"Invalid enpassant target should throw");
 }
 
-- testInitializationWithInsufficientCastlingOptionsThrows() {
+- (void)testInitializationWithInsufficientCastlingOptionsThrows {
 	CPPAssertThrows(new GameState("rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR", sfc::cfw::ColorWhite, "-"), @"Insufficient information in castling string");
 	CPPAssertThrows(new GameState("rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR", sfc::cfw::ColorWhite, "--"), @"Insufficient information in castling string");
 	CPPAssertThrows(new GameState("rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR", sfc::cfw::ColorWhite, "---"), @"Insufficient information in castling string");
 }
 
-- testInitializationWithInvalidCastlingOptionFormatThrows() {
+- (void)testInitializationWithInvalidCastlingOptionFormatThrows {
 	CPPAssertThrows(new GameState("rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR", sfc::cfw::ColorWhite, "1Qin"), @"Invalid castling options format should throw");
 }
 
-- testInitializationWithInapplicableWhiteKingsideCastlingOptionThrows() {
+- (void)testInitializationWithInapplicableWhiteKingsideCastlingOptionThrows {
 	CPPAssertThrows(new GameState("rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR", sfc::cfw::ColorWhite, "GQkq"), @"Inapplicable castling option for white kingside");
 }
 
-- testInitializationWithInapplicableWhiteQueensideCastlingOptionThrows() {
+- (void)testInitializationWithInapplicableWhiteQueensideCastlingOptionThrows {
 	CPPAssertThrows(new GameState("rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/1NBQKBNR", sfc::cfw::ColorWhite, "KAkq"), @"Inapplicable castling option for white queenside");
 }
 
-- testInitializationWithInapplicableBlackKingsideCastlingOptionThrows() {
+- (void)testInitializationWithInapplicableBlackKingsideCastlingOptionThrows {
 	CPPAssertThrows(new GameState("rnbqkbn1/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR", sfc::cfw::ColorWhite, "KQhq"), @"Inapplicable castling option for white kingside");
 }
 
-- testInitializationWithInapplicableBlackQueensideCastlingOptionThrows() {
+- (void)testInitializationWithInapplicableBlackQueensideCastlingOptionThrows {
 	CPPAssertThrows(new GameState("1nbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/1NBQKBNR", sfc::cfw::ColorWhite, "KAkq"), @"Inapplicable castling option for white queenside");
 }
 
-- testInitializationWithWhiteInCheckAndBlackToPlayThrows() {
+- (void)testInitializationWithWhiteInCheckAndBlackToPlayThrows {
 	/// @todo
 }
 
-- testInitializationWithBlackInCheckAndWhiteToPlayThrows() {
+- (void)testInitializationWithBlackInCheckAndWhiteToPlayThrows {
 	/// @todo
 }
 
-- testInitializationWithoutWhiteKingThrows() {
+- (void)testInitializationWithoutWhiteKingThrows {
 	CPPAssertThrows(new GameState("rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQ1BNR", sfc::cfw::ColorWhite, "KQkq"), @"Initialization without white king should throw");
 }
 
-- testInitializationWithoutBlackKingThrows() {
+- (void)testInitializationWithoutBlackKingThrows {
 	CPPAssertThrows(new GameState("rnbq1bnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR", sfc::cfw::ColorWhite, "KQkq"), @"Initialization without black king should throw");
 }
 
-- testInitializationWithMultipleWhiteKingsThrows() {
+- (void)testInitializationWithMultipleWhiteKingsThrows {
 	CPPAssertThrows(new GameState("rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPK/RNBQKBNR", sfc::cfw::ColorWhite, "KQkq"), @"Initialization with multiple white king should throw");
 }
 
-- testInitializationWithMultipleBlackKingsThrows() {
+- (void)testInitializationWithMultipleBlackKingsThrows {
 	CPPAssertThrows(new GameState("rnbqkbnr/pppp1ppp/k7/4p3/4P3/8/PPPP1PPP/RNBQKBNR", sfc::cfw::ColorWhite, "KQkq"), @"Initialization with multiple black king should throw");
 }
 
