57028ea85c37ac70f2dad219134f35e3950262f2
Santhos Baala RS <rssanthosbaala@gmail.com>
2013-07-29 23:10:14 +0530

    Test cases written for pawn without captures.

---
+++ ChessFramework/ChessFramework Tests/PositionQuerierPawnAttack_Tests.mm
@@ -0,0 +1,34 @@
+//
+//  PositionQuerierPawnAttack_Tests.m
+//  ChessFramework
+//
+//  Created by Santhos Baala RS on 29/07/13.
+//  Copyright (c) 2013 64cloud. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PositionQuerierPawnAttack_Tests : XCTestCase
+
+@end
+
+@implementation PositionQuerierPawnAttack_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
